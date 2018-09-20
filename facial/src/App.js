c1040f89bc6242f9409ea21324946a3179cfef32
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-20 11:39:33 +1000

    working on logo

--- facial/src/App.js
+++ facial/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Navigation />
-     {/* { <Logo />
-       <ImageLinkForm />
+       <Logo />
+     {/*   <ImageLinkForm />
        <FaceRecognition />}*/}
       </div>
     );
