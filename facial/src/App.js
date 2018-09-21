08e0d4bcfe9489cbbfddb49a76a24ec3c6ff593c
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-21 13:27:24 +1000

    added react particles, code with rank system with css changes to font-family

--- facial/src/App.js
+++ facial/src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 
+const particlesOptions = {
+                particles: {
+                  line_linked: {
+                    shadow: {
+                      enable: true,
+                      color: "#3CA9D1",
+                      blur: 5
+                    }
+                  }
+                }
+              }
+            
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Particles className='particles'
+              params={particlesOptions}
+            />
        <Navigation />
        <Logo />
+       <Rank/>
       <ImageLinkForm />
        {/*<FaceRecognition />}*/}
       </div>
