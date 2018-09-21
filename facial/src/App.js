942c2356ad7b42f1024df81f8f82614d8cfbe03f
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-21 16:38:39 +1000

    adding more features and clarifai api

--- facial/src/App.js
+++ facial/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+
+const app = new Clarifai.App({
+ apiKey: 'e67f6fe6e99c435cad3c236e77d74d96'
+});
 
 const particlesOptions = {
                 particles: {
@@ -28,6 +34,31 @@ const particlesOptions = {
           
             
 class App extends Component {
+  constructor (){
+    super();
+    this.state={
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+    function(response) {
+      // do something with response
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
   render() {
     return (
       <div className="App">
@@ -37,8 +68,10 @@ class App extends Component {
        <Navigation />
        <Logo />
        <Rank/>
-      <ImageLinkForm />
-       {/*<FaceRecognition />}*/}
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
+       <FaceRecognition />
       </div>
     );
   }
---
+++ facial/src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const FaceRecognition = () =>{
+	return(
+		<div className='center'>
+		<img alt="photo display" src={'https://samples.clarifai.com/face-det.jpg'}/>
+
+			
+		</div>
+	); 
+}
+
+export default FaceRecognition;
